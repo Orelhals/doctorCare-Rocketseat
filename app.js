3176745eef5e45bd5ac2df778df876f64552c68b
Arthur Pimenta <arthurstt22@gmail.com>
2022-05-12 21:01:22 -0300

    Scroll effect

--- app.js
+++ app.js
@@ -29,5 +29,31 @@ const abrirMenu = () => {
 window.abrirMenu = abrirMenu;
 
 
+// Animação aso Scroll 
+
+// Eventos
+window.addEventListener('scroll', animaScroll);
+
+// Funções 
+
+function animaScroll() {
+    sections.forEach(section => {
+        const topo = section.getBoundingClientRect().top;
+        const isVisible = (topo - metadePage) < 0;
+        if (isVisible) {
+            section.classList.add('ativo')
+        }
+
+    });
+
+}
+
+// Variáveis 
+
+const sections = document.querySelectorAll('.js-scroll');
+const metadePage = window.innerHeight * 0.6;
+
+
+
 
 
